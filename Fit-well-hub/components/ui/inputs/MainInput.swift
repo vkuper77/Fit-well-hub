1f07e19c0cd2971791ee9ecb48038e58ddeceb8b
vitali <vkuper7@gmail.com>
2023-12-16 23:40:08 +0300

    feat: add registration screen and UI components

---
+++ Fit-well-hub/components/ui/inputs/MainInput.swift
@@ -0,0 +1,18 @@
+//
+//  MainInput.swift
+//  Fit-well-hub
+//
+//  Created by Vitali Kupratsevich on 16.12.23.
+//
+
+import SwiftUI
+
+struct MainInput: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainInput()
+}
