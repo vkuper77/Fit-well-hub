899b49adeedfadfb6eda566d8814c3e592372250
vitali <vkuper7@gmail.com>
2024-01-21 23:22:08 +0300

    feat: add code input

---
+++ Fit-well-hub/views/ui/inputs/CodeInput.swift
@@ -0,0 +1,18 @@
+//
+//  CodeInput.swift
+//  Fit-well-hub
+//
+//  Created by Vitali Kupratsevich on 21.01.24.
+//
+
+import SwiftUI
+
+struct CodeInput: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CodeInput()
+}
